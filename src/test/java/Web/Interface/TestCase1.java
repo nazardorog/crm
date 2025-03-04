60eda8735ece6c93146eb093179f27e455bc05fa
Administrator <nazar.mykytynskiy@geekqlick.com>
2025-03-04 12:53:31 +0000

    1. Логин на веб

---
+++ src/test/java/Web/Interface/TestCase1.java
@@ -0,0 +1,39 @@
+package Web.Interface;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class TestCase1 {
+    public String webSite = "https://preprod.empirenational.com/adm";
+
+    @BeforeTest
+
+    public void runBrowser() {
+        Configuration.browser = "chrome";
+        System.setProperty("webdriver.chrome.driver", "C:/automation/chromedriver-win64/141/chromedriver.exe");
+        Selenide.open(webSite);
+    }
+
+    @Test
+    public void loginWeb() throws InterruptedException {
+        $("#loginform-username").setValue("test1te");
+        $("#loginform-password").setValue("t34n2215P39L");
+        $(".btn.btn-primary.btn-block.btn-flat").click();
+
+        String screenshotPath = Selenide.screenshot("screenTestEmpire");
+        System.out.println("test passed");
+        System.out.println("screenshot saved: " + screenshotPath);
+        Thread.sleep(5000);
+        System.out.println("Test1 - OK");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
+}
