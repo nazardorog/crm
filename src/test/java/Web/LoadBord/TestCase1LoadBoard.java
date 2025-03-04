550388178c8294686aa2498a741c21f5dfe73af5
Administrator <nazar.mykytynskiy@geekqlick.com>
2025-03-04 12:49:36 +0000

    1. Логин на веб

--- src/test/java/Web/LoadBord/TestCase1LoadBoard.java
+++ src/test/java/Web/LoadBord/TestCase1LoadBoard.java
@@ -1,5 +1,6 @@
 package Web.LoadBord;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.testng.annotations.AfterTest;
@@ -7,10 +8,12 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class TestCase1LoadBoard {
     public String webSite = "https://preprod.empirenational.com/adm";
@@ -32,28 +35,69 @@ public class TestCase1LoadBoard {
 
         $("#new_load").click();
 
+        LocalDateTime now = LocalDateTime.now();
+        int day = now.getDayOfMonth();
+        int hour = now.getHour();
+        int minute = (now.getMinute() / 5) * 5;
+
+        //brocker
         $("#select2-broker_search-container").click();
         $(".select2-search__field").setValue("Auto test broker");
         $(".select2-results__options").shouldHave(text("LOADS DONE: 1")).click();
+        $$("select#loads-agent_id option").findBy(text("Auto test agent")).click();
 
+        //shippers pickup
         $("#select2-shippers-receiver-origin-container").click();
         $(".select2-search__field").setValue("Auto test shipper 1");
         $(".select2-results").shouldHave(text("Auto test shipper 1")).click();
-        $(".btn btn-default kv-datetime-picker").click();
+
+        //calendar shippers pickup from
+        $("#loadspickuplocations-0-date_from-datetime .kv-datetime-picker").click();
+        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day))).click(); // Вибираємо день
+        $$(".datetimepicker-hours .hour").findBy(exactText(hour + ":00")).click(); // Вибираємо годину
+        $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour, minute))).click(); // Вибираємо хвилини
+
+        //calendar shippers pickup to
+        $("#loadspickuplocations-0-date_to-datetime .kv-datetime-picker").click();
+        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day))).click(); // Вибираємо день
+        $$(".datetimepicker-hours .hour").findBy(exactText(hour + 3 + ":00")).click(); // Вибираємо годину
+        $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour + 3, minute))).click(); // Вибираємо хвилини
+
+        $("#loadspickuplocations-0-weight").setValue("1");
+        $("#loadspickuplocations-0-pallets").setValue("1");
+        $("#loadspickuplocations-0-pcs").setValue("1");
 
         $("#select2-shippers-receiver-destination-container").click();
         $(".select2-search__field").setValue("Auto test shipper 2");
         $(".select2-results__options").shouldHave(text("Auto test shipper 2")).click();
 
+        //calendar shippers destination from
+        $("#loadsdeliverylocations-0-date_from-datetime .kv-datetime-picker").click();
+        Thread.sleep(3000);
+        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day+1))).click(); // Вибираємо день
+        $$(".datetimepicker-hours .hour").findBy(exactText(hour + ":00")).click(); // Вибираємо годину
+        $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour, minute))).click();
+
+        //calendar shippers destination to
+        $("#loadsdeliverylocations-0-date_to-datetime .kv-datetime-picker").click();
+        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day+1))).click(); // Вибираємо день
+        $$(".datetimepicker-hours .hour").findBy(exactText(hour + ":00")).click(); // Вибираємо годину
+        $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour, minute))).click();
+
+        $("#loadsdeliverylocations-0-pallets").setValue("1");
+        $("#loadsdeliverylocations-0-weight").setValue("1");
+        $("#loadsdeliverylocations-0-pcs").setValue("1");
+
         $("#loads-reference").setValue("1122334");
         $("#loads-rate-disp").setValue("100000").pressEnter();
         $("#loads-carrier_rate-disp").setValue("80000").pressEnter();
 
-//        $("#loads-pallets").setValue("1");
-//        $("#loads-weight").setValue("100");
-//        $(".btn.btn-primary.btn-block.btn-flat").click();
+        $("#backup-form-create").scrollIntoView(true);
+        $("#backup-form-create").click();
 
     }
+
+
 
     @AfterTest
     public void tearDown() {
