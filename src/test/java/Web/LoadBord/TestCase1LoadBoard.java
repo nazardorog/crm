208f01e799c9f8e69e5a43d79c3c0657691565f9
Administrator <nazar.mykytynskiy@geekqlick.com>
2025-03-05 14:20:40 +0000

    1. Создание New Load / 1 ПУ 1 ДЕЛ
    создание 4х грузов где в каждом своей тип документа
    -BOL
    -Rate Con
    -POD
    -Other (единственный тип для загрузки фото)

--- src/test/java/Web/LoadBord/TestCase1LoadBoard.java
+++ src/test/java/Web/LoadBord/TestCase1LoadBoard.java
@@ -3,36 +3,24 @@ package Web.LoadBord;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.files.DownloadActions.click;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestCase1LoadBoard {
-    public String webSite = "https://preprod.empirenational.com/adm";
 
-    @BeforeTest
-    public void runBrowser() {
-        Configuration.browser = "chrome";
-        System.setProperty("webdriver.chrome.driver", "C:/automation/chromedriver-win64/141/chromedriver.exe");
-        Selenide.open(webSite);
-    }
-
-    @Test
-    public void loginWeb() throws InterruptedException {
-        $("#loginform-username").setValue("test1te");
-        $("#loginform-password").setValue("t34n2215P39L");
-        $(".btn.btn-primary.btn-block.btn-flat").click();
+    @Test(dependsOnMethods = {"Web.TestCase1.loginWeb"})
+    public void addNewLoad() throws InterruptedException {
 
         $(".logo-mini-icon").shouldBe(visible, Duration.ofSeconds(10));
-
         $("#new_load").click();
 
         LocalDateTime now = LocalDateTime.now();
@@ -41,9 +29,9 @@ public class TestCase1LoadBoard {
         int minute = (now.getMinute() / 5) * 5;
 
         //brocker
-        $("#select2-broker_search-container").click();
+        $("#select2-broker_search-container").shouldBe(Condition.visible, Condition.enabled).click();
         $(".select2-search__field").setValue("Auto test broker");
-        $(".select2-results__options").shouldHave(text("LOADS DONE: 1")).click();
+        $(".select2-results__options").shouldHave(text("Auto test broker")).click();
         $$("select#loads-agent_id option").findBy(text("Auto test agent")).click();
 
         //shippers pickup
@@ -53,15 +41,15 @@ public class TestCase1LoadBoard {
 
         //calendar shippers pickup from
         $("#loadspickuplocations-0-date_from-datetime .kv-datetime-picker").click();
-        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day))).click(); // Вибираємо день
+        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day + 1))).click(); // Вибираємо день
         $$(".datetimepicker-hours .hour").findBy(exactText(hour + ":00")).click(); // Вибираємо годину
         $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour, minute))).click(); // Вибираємо хвилини
 
         //calendar shippers pickup to
         $("#loadspickuplocations-0-date_to-datetime .kv-datetime-picker").click();
-        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day))).click(); // Вибираємо день
-        $$(".datetimepicker-hours .hour").findBy(exactText(hour + 3 + ":00")).click(); // Вибираємо годину
-        $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour + 3, minute))).click(); // Вибираємо хвилини
+        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day + 1))).click(); // Вибираємо день
+        $$(".datetimepicker-hours .hour").findBy(exactText(hour + 6 + ":00")).click(); // Вибираємо годину
+        $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour + 6, minute))).click(); // Вибираємо хвилини
 
         $("#loadspickuplocations-0-weight").setValue("1");
         $("#loadspickuplocations-0-pallets").setValue("1");
@@ -73,16 +61,15 @@ public class TestCase1LoadBoard {
 
         //calendar shippers destination from
         $("#loadsdeliverylocations-0-date_from-datetime .kv-datetime-picker").click();
-        Thread.sleep(3000);
-        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day+1))).click(); // Вибираємо день
+        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day + 2))).click(); // Вибираємо день
         $$(".datetimepicker-hours .hour").findBy(exactText(hour + ":00")).click(); // Вибираємо годину
         $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour, minute))).click();
 
         //calendar shippers destination to
         $("#loadsdeliverylocations-0-date_to-datetime .kv-datetime-picker").click();
-        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day+1))).click(); // Вибираємо день
-        $$(".datetimepicker-hours .hour").findBy(exactText(hour + ":00")).click(); // Вибираємо годину
-        $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour, minute))).click();
+        $$(".datetimepicker-days .day").findBy(exactText(String.valueOf(day + 2))).click(); // Вибираємо день
+        $$(".datetimepicker-hours .hour").findBy(exactText(hour + 6 + ":00")).click(); // Вибираємо годину
+        $$(".datetimepicker-minutes .minute").findBy(exactText(String.format("%d:%02d", hour + 6, minute))).click();
 
         $("#loadsdeliverylocations-0-pallets").setValue("1");
         $("#loadsdeliverylocations-0-weight").setValue("1");
@@ -92,18 +79,68 @@ public class TestCase1LoadBoard {
         $("#loads-rate-disp").setValue("100000").pressEnter();
         $("#loads-carrier_rate-disp").setValue("80000").pressEnter();
 
-        $("#backup-form-create").scrollIntoView(true);
-        $("#backup-form-create").click();
+    //load file
+        Thread.sleep(4000);
+        SelenideElement modal = $("#add_load");
+        executeJavaScript("arguments[0].scrollTop = arguments[0].scrollHeight;", modal);
+
+        $("#add_load").find(".modal-footer-button .fa-files-o").click();
+
+        executeJavaScript("arguments[0].scrollTop = 0;", modal);
+        SelenideElement targetElement = $(".btn-file");
+        File file = new File("C:/Empire/pdf1.pdf");
+        $("#loaddocuments-0-file").uploadFile(file);
+
+        if (!$("#loaddocuments-0-type").isDisplayed()){ //scroll
+            scrollDown($("#add_load"), $("#loaddocuments-0-type"));
+        }
+        $("#loaddocuments-0-type").selectOption("BOL");
+
+        if (!$("#load_documents_modal_pseudo_submit").isDisplayed()){ //scroll
+            scrollDown($("#add_load"), $("#load_documents_modal_pseudo_submit"));
+        }
+        $("#load_documents_modal_pseudo_submit").click();
+
+        executeJavaScript("arguments[0].scrollTop = arguments[0].scrollHeight;", modal); //scroll
+        $("#add_load_send_old").click();
+
+        $("#select2-load_truck_id-0-container").shouldBe(Condition.visible, Condition.enabled).click();
+
+    //dispatch board
+        $(".select2-search__field").setValue("0303");
+        $(".select2-results__option--highlighted").shouldHave(text("0303")).click();
+
+        if (!$("#loads-load_type label").isDisplayed()){ //scroll
+            scrollDown($("#add_load"), $("#loads-load_type label"));
+        }
+        $$("#loads-load_type label").findBy(Condition.text("Board")).click();
+
+        executeJavaScript("arguments[0].scrollTop = arguments[0].scrollHeight;", modal); //scroll
+        $("#dispatch_load_send").click();
+
+        boolean statusDriver = $("#set-automatic-status-link").isDisplayed();
+        if (statusDriver){
+            $("#set-automatic-status-link").shouldHave(text("please set the automatic status change to Available On")).click();
+            $(".text-set-status-link").shouldHave(text("Please note that the pick-up for this load is scheduled for  Thursday. As a result, the truck's status will automatically change to 'Available On' in the delivery city at 12:01 AM on the pick-up day."));
+            $("#automatic_status_send]").click();
+            $("#dispatch_load_send").click();
+        }
 
     }
 
+    public void scrollUp(SelenideElement modal, SelenideElement target){
 
-
-    @AfterTest
-    public void tearDown() {
-        Selenide.closeWebDriver();
+        while (!target.isDisplayed()) {
+            executeJavaScript("arguments[0].scrollTop -= 100;", modal); // Прокрутка вверх на 100 пікселів
+            sleep(500);
+        }
     }
 
+    public void scrollDown(SelenideElement modal, SelenideElement target) {
 
-
+        while (!target.isDisplayed()) {
+            executeJavaScript("arguments[0].scrollTop += 100;", modal); // Прокрутка вниз на 100 пікселів
+            sleep(500);
+        }
+    }
 }
