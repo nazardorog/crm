38ff69a01f793b049f6b8e5a6f2f7a58f2cc0896
Administrator <nazar.mykytynskiy@geekqlick.com>
2025-03-17 12:57:27 +0200

    5. Создание/Редактирование брокера

---
+++ src/test/java/Web/LoadBord/TestCase13LoadBoard.java
@@ -0,0 +1,98 @@
+package Web.LoadBord;
+
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Random;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class TestCase13LoadBoard {
+
+    @Test(dependsOnMethods = {"Web.Login.loginWeb"})
+    public void newBrocker() throws InterruptedException{
+
+        //прибрати віджет чат
+        executeJavaScript("document.querySelector('.chat-widget').style.display='none'");
+
+        //генеруємо дані для створення брокера
+        Random random = new Random();
+        String brokerMcNumber = "" + random.nextInt(10000000);
+        String brokerName = "Auto test broker" + String.format("%02d",random.nextInt(100));
+        String brokerPhoneNumber = "(056) 333-" + String.format("%04d", random.nextInt(10000));
+        String agentName = "Auto test agent" + String.format("%02d",random.nextInt(100));
+        String agentMail = "AutoTestAgent" + String.format("%02d", random.nextInt(100))+ "@mail.com";
+        String agentPhoneNumber = "(056) 334" + String.format("%04d", random.nextInt(10000)) + "01";
+
+        //відкриваємо New Load
+        $(".logo-mini-icon").shouldBe(visible, Duration.ofSeconds(20));
+        $("#new_load").click();
+
+        //створюємо Broker
+        $("#new_broker").click();
+        $("#brokers-mc_number").setValue(brokerMcNumber);
+        $("#brokers-name").setValue(brokerName);
+        $("#brokers-address").setValue("Mountain");
+        $("#brokers-city").setValue("Colorado");
+
+        //вкаладка Contact
+        $(byText("Contact")).click();
+        $("#brokers-main_phone_number").setValue(brokerPhoneNumber);
+
+        //вкладка Agents
+        $(byText("Agents")).click();
+        $("#agents-0-name").setValue(agentName);
+        $("#agents-0-email").setValue(agentMail);
+        $("#agents-0-phone_number").setValue(agentPhoneNumber);
+        $(".btn-update-broker-agents .btn-primary ").click();
+
+        //перевіряємо додавання свтореного Агента на фреймі New Load під полем Broker
+        $(".bt-load-broker-main-flex").shouldHave(text(brokerName));
+        $(".bt-load-broker-main-flex").shouldHave(text("Mountain"));
+        $(".bt-load-broker-main-flex").shouldHave(text("Colorado"));
+        $(".bt-load-broker-main-flex").shouldHave(text(brokerPhoneNumber));
+
+        //генеруємо дані для редагування Брокера
+        $(".broker_buttons .glyphicon-pencil").click();
+        String editBrokerMcNumber = "" + random.nextInt(10000000);
+        String editBrokerName = "Auto test broker" + String.format("%02d",random.nextInt(100));
+        String editBrokerPhoneNumber = "(056) 333-" + String.format("%04d", random.nextInt(10000));
+        String editAgentName = "Auto test agent" + String.format("%02d",random.nextInt(100));
+        String editAgentMail = "AutoTestAgent" + String.format("%02d", random.nextInt(100))+ "@mail.com";
+        String editAgentPhoneNumber = "(056) 334" + String.format("%04d", random.nextInt(10000)) + "01";
+
+        //вводимо відредаговані дані Брокера
+        $("#brokers-mc_number").setValue(editBrokerMcNumber);
+        $("#brokers-name").setValue(editBrokerName);
+        $("#brokers-address").setValue("Texas");
+        $("#brokers-city").setValue("Dallas");
+
+        //вкаладка Contact
+        $(byText("Contact")).click();
+        $("#brokers-main_phone_number").setValue(editBrokerPhoneNumber);
+
+        //вкладка Agents
+        $(byText("Agents")).click();
+        $(".edit_agent_btn").click();
+        $("#agents-0-name").setValue(editAgentName);
+        $("#agents-0-email").setValue(editAgentMail);
+        $("#agents-0-phone_number").setValue(editAgentPhoneNumber);
+        $(".btn-update-broker-agents .btn-primary ").click();
+
+        //перевіряємо відредаговані дані брокера
+        $(".bt-load-broker-main-flex").shouldHave(text(editBrokerName));
+        $(".bt-load-broker-main-flex").shouldHave(text("Texas"));
+        $(".bt-load-broker-main-flex").shouldHave(text("Dallas"));
+        $(".bt-load-broker-main-flex").shouldHave(text(editBrokerPhoneNumber));
+
+        //повертаємось на Load bord
+        $("#add_load .close").click();
+        $(".logo-mini-icon").shouldBe(visible, Duration.ofSeconds(10)).click();
+
+        System.out.println("TestCase13LoadBoard - OK");
+    }
+}
