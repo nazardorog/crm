ffeee5e2a56b2e3652cb6f7464fe785307824d17
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-04-11 16:28:39 +0300

    виправлення

--- src/test/java/web/bigTruck/BigTruckTestCase3LoadBoard.java
+++ src/test/java/web/bigTruck/BigTruckTestCase3LoadBoard.java
@@ -33,102 +33,100 @@ public class BigTruckTestCase3LoadBoard extends LoginUser2 {
     @Test
     public void availableCargoToEnRout () throws InterruptedException {
 
-//        System.out.println("BigTruckTestCase3LoadBoard - Start");
-//
-//        //створює новий вантаж
-//        $(".logo-mini-icon").shouldBe(enabled, Duration.ofSeconds(30)).click();
-//        $("#new_load").shouldBe(enabled).click();
-//
-//        //brocker
-//        $("#loads-form-create").shouldBe(visible, Duration.ofSeconds(10));
-//        $("#select2-broker_search-container").shouldBe(visible).click();
-//        $(".select2-search__field").shouldBe(visible).setValue("Auto test broker");
-//        $$(".select2-results__options")
-//                .findBy(text("Auto test broker"))
-//                .click();
-//        $("#select2-broker-agent-load-select-container").click();
-//        $(".select2-search__field").setValue("Auto test agent");
-//        $$(".select2-results__options")
-//                .findBy(text("Auto test agent"))
-//                .click();
-//
-//        //input other data
-//        Random random = new Random();
-//        String reference = String.format("%3d", random.nextInt(10000000));
-//        String commodity = String.format("%3d", random.nextInt(10000000));
-//        $("#loads-reference").setValue(reference);
-//        $("#loads-rate-disp").setValue("100000").pressEnter();
-//        $("#loads-commodity_big_trucks_note").setValue(commodity);
-//        $("#select2-booked_with-container").shouldHave(text("Auto 2Test BT"));
-//        $("#loads-commodity").setValue("Text Commodity Notes");
-//        $$("div#loads-check_full_load label").findBy(text("FTL")).click();
-//        $$("#loads-local_type label").findBy(text("Local")).click();
-//        $$("#loads-load_type label").findBy(text("Board")).click();
-//
-//        //load file
-//        $(".load_documents_counter-flex").click();
-//        $("#select2-loaddocuments-0-type-container").click();
-//        $$(".select2-results__option").findBy(text("Rate confirmation")).click();
-//
-//        File file = new File("C:/Empire/pdf1.pdf");
-//        $("#loaddocuments-0-file").uploadFile(file);
-//        $("#load_documents_modal_pseudo_submit").click();
-//
-//        //вкладка Origin & Destination
-//        $("#origin-destination-tab").click();
-//
-//        //Origin Shippers
-//        $("#select2-shippers-receiver-origin-container").shouldBe(enabled).click();
-//        $(".select2-search__field").setValue("Auto test shipper 1");
-//        $$("li.select2-results__option")
-//                .findBy(text("Auto test shipper 1"))
-//                .click();
-//
-//        //Destination Shippers
-//        $("#select2-shippers-receiver-destination-container").shouldBe(enabled).click();
-//        $(".select2-search__field").setValue("Auto test shipper 2");
-//        $$("li.select2-results__option")
-//                .findBy(text("Auto test shipper 2"))
-//                .click();
-//
-//        //calendar Origin Shippers Date from
-//        $("#loadspickuplocations-0-date_from-datetime .kv-datetime-picker").click();
-//        inputCalendar(1, 0);
-//
-//        //calendar Origin Shippers Date to
-//        $("#loadspickuplocations-0-date_to-datetime .kv-datetime-picker").click();
-//        inputCalendar(2, 1);
-//
-//        //calendar Destination Shippers Date from
-//        $("#loadsdeliverylocations-0-date_from-datetime .kv-datetime-picker").click();
-//        inputCalendar(3, 2);
-//
-//        //calendar Destination Shippers Date to
-//        $("#loadsdeliverylocations-0-date_to-datetime .kv-datetime-picker").click();
-//        inputCalendar(4, 3);
-//
-//        //pallets Origin Shippers
-//        $("#loadspickuplocations-0-pallets").setValue("1");
-//        $("#loadspickuplocations-0-weight").setValue("1");
-//        $("#loadspickuplocations-0-pcs").setValue("1");
-//
-//        //pallets Destination Shippers
-//        $("#loadsdeliverylocations-0-pallets").setValue("1");
-//        $("#loadsdeliverylocations-0-weight").setValue("1");
-//        $("#loadsdeliverylocations-0-pcs").setValue("1");
-//
-//        //зберігає номер вантажу на фрейм Dispatch board
-//        $("#add_load_send_dispatch").click();
-//        $("#view_load").shouldBe(visible).shouldHave(text("Dispatch #"));
-//
-//        //отримує номер вантажу
-//        String loadNumber = $("#view_load .check_call_pro").getText();
-//        System.out.println("BigTruckTestCase3LoadBoard. Номер вантажу:" + loadNumber);
-//
-//        //закриває модальне вікно Dispatch Load
-//        $(".load-info-modal-dialog .close").shouldBe(enabled).click();
+        System.out.println("BigTruckTestCase3LoadBoard - Start");
 
-        String loadNumber = "70769";
+        //створює новий вантаж
+        $(".logo-mini-icon").shouldBe(enabled, Duration.ofSeconds(30)).click();
+        $("#new_load").shouldBe(enabled).click();
+
+        //brocker
+        $("#loads-form-create").shouldBe(visible, Duration.ofSeconds(10));
+        $("#select2-broker_search-container").shouldBe(visible).click();
+        $(".select2-search__field").shouldBe(visible).setValue("Auto test broker");
+        $$(".select2-results__options")
+                .findBy(text("Auto test broker"))
+                .click();
+        $("#select2-broker-agent-load-select-container").click();
+        $(".select2-search__field").setValue("Auto test agent");
+        $$(".select2-results__options")
+                .findBy(text("Auto test agent"))
+                .click();
+
+        //input other data
+        Random random = new Random();
+        String reference = String.format("%3d", random.nextInt(10000000));
+        String commodity = String.format("%3d", random.nextInt(10000000));
+        $("#loads-reference").setValue(reference);
+        $("#loads-rate-disp").setValue("100000").pressEnter();
+        $("#loads-commodity_big_trucks_note").setValue(commodity);
+        $("#select2-booked_with-container").shouldHave(text("Auto 2Test BT"));
+        $("#loads-commodity").setValue("Text Commodity Notes");
+        $$("div#loads-check_full_load label").findBy(text("FTL")).click();
+        $$("#loads-local_type label").findBy(text("Local")).click();
+        $$("#loads-load_type label").findBy(text("Board")).click();
+
+        //load file
+        $(".load_documents_counter-flex").click();
+        $("#select2-loaddocuments-0-type-container").click();
+        $$(".select2-results__option").findBy(text("Rate confirmation")).click();
+
+        File file = new File("C:/Empire/pdf1.pdf");
+        $("#loaddocuments-0-file").uploadFile(file);
+        $("#load_documents_modal_pseudo_submit").click();
+
+        //вкладка Origin & Destination
+        $("#origin-destination-tab").click();
+
+        //Origin Shippers
+        $("#select2-shippers-receiver-origin-container").shouldBe(enabled).click();
+        $(".select2-search__field").setValue("Auto test shipper 1");
+        $$("li.select2-results__option")
+                .findBy(text("Auto test shipper 1"))
+                .click();
+
+        //Destination Shippers
+        $("#select2-shippers-receiver-destination-container").shouldBe(enabled).click();
+        $(".select2-search__field").setValue("Auto test shipper 2");
+        $$("li.select2-results__option")
+                .findBy(text("Auto test shipper 2"))
+                .click();
+
+        //calendar Origin Shippers Date from
+        $("#loadspickuplocations-0-date_from-datetime .kv-datetime-picker").click();
+        inputCalendar(1, 0);
+
+        //calendar Origin Shippers Date to
+        $("#loadspickuplocations-0-date_to-datetime .kv-datetime-picker").click();
+        inputCalendar(2, 1);
+
+        //calendar Destination Shippers Date from
+        $("#loadsdeliverylocations-0-date_from-datetime .kv-datetime-picker").click();
+        inputCalendar(3, 2);
+
+        //calendar Destination Shippers Date to
+        $("#loadsdeliverylocations-0-date_to-datetime .kv-datetime-picker").click();
+        inputCalendar(4, 3);
+
+        //pallets Origin Shippers
+        $("#loadspickuplocations-0-pallets").setValue("1");
+        $("#loadspickuplocations-0-weight").setValue("1");
+        $("#loadspickuplocations-0-pcs").setValue("1");
+
+        //pallets Destination Shippers
+        $("#loadsdeliverylocations-0-pallets").setValue("1");
+        $("#loadsdeliverylocations-0-weight").setValue("1");
+        $("#loadsdeliverylocations-0-pcs").setValue("1");
+
+        //зберігає номер вантажу на фрейм Dispatch board
+        $("#add_load_send_dispatch").click();
+        $("#view_load").shouldBe(visible).shouldHave(text("Dispatch #"));
+
+        //отримує номер вантажу
+        String loadNumber = $("#view_load .check_call_pro").getText();
+        System.out.println("BigTruckTestCase3LoadBoard. Номер вантажу:" + loadNumber);
+
+        //закриває модальне вікно Dispatch Load
+        $(".load-info-modal-dialog .close").shouldBe(enabled).click();
 
         //перевіряє що вантаж відображається на Available Loads
         $(".logo-mini-icon").shouldBe(enabled, Duration.ofSeconds(30)).click();
@@ -140,20 +138,20 @@ public class BigTruckTestCase3LoadBoard extends LoginUser2 {
 
 
 
-//        //перевіряє що вантаж створено в Load bord вводить номер вантажу і перевіряє що він є в таб частині
-//        $(".logo-mini-icon").shouldBe(enabled, Duration.ofSeconds(30)).click();
-//        $$("#loadTabs .updated-tabs-name-link")
-//                .findBy(text("Available Loads:"))
-//                .click();
-//
-//        $("#available-loads-grid-filters .form-control")
-//                .shouldBe(visible, enabled)
-//                .setValue(loadNumber).pressEnter();
-//
-//        $$("table#available-loads-grid tbody tr").shouldHave(CollectionCondition.size(1));
-//        $("td a.view_load")
-//                .shouldHave(text(loadNumber))
-//                .shouldBe(visible);
+        //перевіряє що вантаж створено в Load bord вводить номер вантажу і перевіряє що він є в таб частині
+        $(".logo-mini-icon").shouldBe(enabled, Duration.ofSeconds(30)).click();
+        $$("#loadTabs .updated-tabs-name-link")
+                .findBy(text("Available Loads:"))
+                .click();
+
+        $("#available-loads-grid-filters .form-control")
+                .shouldBe(visible, enabled)
+                .setValue(loadNumber).pressEnter();
+
+        $$("table#available-loads-grid tbody tr").shouldHave(CollectionCondition.size(1));
+        $("td a.view_load")
+                .shouldHave(text(loadNumber))
+                .shouldBe(visible);
 
         //клік на око, редагування вантажу Dispatch load
         $("#available-loads-grid button.view_load").click();
