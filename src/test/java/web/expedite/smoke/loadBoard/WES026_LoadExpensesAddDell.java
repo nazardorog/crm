3db6d4e6a69da3a1c225aa6580ea0d84ce599ad8
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-05-23 12:03:42 +0300

    B Revert "WES026"
    This reverts commit fcd918619b095032717a59c6893b73d86db873e4.

--- src/test/java/web/expedite/smoke/loadBoard/WES026_LoadExpensesAddDell.java
+++ src/test/java/web/expedite/smoke/loadBoard/WES026_LoadExpensesAddDell.java
@@ -2,10 +2,7 @@ package web.expedite.smoke.loadBoard;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import utilsWeb.commonWeb.Calendar;
-import utilsWeb.commonWeb.CloseWebDriver;
 import utilsWeb.commonWeb.NewLoadExpedite;
 import utilsWeb.configWeb.GlobalLogin;
 
@@ -19,21 +16,25 @@ public class WES026_LoadExpensesAddDell {
 
     // Click Up:
     // CRM EXPEDITE - Smoke - Loadboard
-    // 15. Dispatch/Add expenses
+    // 14. Dispatch/Add warehouse
 
     @Test
     public void expensesAddDell() {
 
-        // Login. Create new load expedite
+        // Login
         GlobalLogin.login("exp_disp1");
-        String loadNumber = NewLoadExpedite.loadExpedite();
+
+        // Create new load expedite
+//        String loadNumber = NewLoadExpedite.loadExpedite();
+        String loadNumber = "34726";
 
         // Data for test
-        final String atName = "Expenses Name";
-        final String atLocation = "Expenses Location";
+        final String atName = "Name";
+        final String atLocation = "Location";
         final String atAmount = "10000";
+        final String atContactPersonName = "contact AutoTestWareHouses1";
 
-        // Перевіряє Load створено в таблиці
+        // Перевіряє створений Load в таблиці
         $("input[name='LoadsSearch[our_pro_number]']").shouldBe(visible).setValue(loadNumber).pressEnter();
         SelenideElement rowLoad = $$("table.table-striped tbody tr").get(0).shouldHave(text(loadNumber));
         rowLoad.$("td a.view_load").shouldHave(text(loadNumber), EXPECT_5);
@@ -43,56 +44,9 @@ public class WES026_LoadExpensesAddDell {
         $(".load-info-modal-dialog").shouldBe(visible, EXPECT_GLOBAL);
 
         // Додає Expenses
+        int row = $$(".dispatch-viewing-border").size();
+        System.out.println(row);
         $$(".dispatch-viewing-border").findBy(text("Expenses")).click();
         $("#add_expenses").shouldBe(visible, EXPECT_GLOBAL);
-
-        // Вводить дані фрейм Add expenses
-        $("#loadexpenses-name").setValue(atName);
-
-        // Calendar Start Date
-        $("#loadexpenses-start_date-datetime .kv-datetime-picker").click();
-        Calendar.setDateTime(0);
-
-        // Input Amount. Location
-        $("#loadexpenses-amount-disp").setValue(atAmount);
-        $("#loadexpenses-location").setValue(atLocation);
-
-        // Submit frame Add expenses
-        $("#update_load_expenses_send").click();
-        $("#add_expenses").shouldNotBe(visible, EXPECT_GLOBAL);
-
-        // Toast massage
-        $("#toast-container").shouldBe(visible, EXPECT_GLOBAL);
-        $(".toast-message").shouldHave(visible, EXPECT_GLOBAL).shouldHave(text("Other expenses sucessfully added"));
-        $("#toast-container").shouldNotHave(visible, EXPECT_GLOBAL);
-
-        // Перевіряє Expenses додано на фрейм Dispatch
-        $("#loadDriversContent table.table-modal-get-drivers").should(appear);
-        SelenideElement expensesDispatch = $$("#loadDriversContent table.table-modal-get-drivers tbody tr").get(1);
-        expensesDispatch.$$("td").findBy(text("Fees")).shouldBe(visible);
-        expensesDispatch.$$("td").findBy(text("+$100.00")).shouldBe(visible);
-
-        // Перевіряє суми Expenses фрейм Dispatch
-        ElementsCollection expenses = $$(".broker-rate-block-flex");
-        expenses.get(0).shouldHave(text("$1,000.00"));
-        expenses.get(2).shouldHave(text("$800.00"));
-        expenses.get(3).shouldHave(text("$900.00"));
-
-        // Видаляє Expenses перевіряє що видалено
-        expensesDispatch.$(".icon-close-dispatch-name").click();
-        expensesDispatch.shouldNotBe(visible, EXPECT_5);
-
-        // Перевіряє суми після видалення Expenses фрейм Dispatch
-        expenses.get(2).shouldHave(text("$800.00"));
-        expenses.get(3).$(".broker-rate-sum").shouldHave(exactText(""));
-
-        // Закриває фрейм Dispatch
-        $("#view_load button.close").shouldBe(visible, EXPECT_5).click();
-        $("#view_load").shouldNotBe(visible, EXPECT_GLOBAL);
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public void closeWebDriver() {
-        CloseWebDriver.tearDown();
     }
 }
