2a9a439a052abb3309d93424f9244b996ba29983
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-06-20 10:48:47 +0300

    2

--- src/test/java/web/expedite/ui/WEU002_Broker.java
+++ src/test/java/web/expedite/ui/WEU002_Broker.java
@@ -25,7 +25,6 @@ public class WEU002_Broker {
         $(".logo-mini-icon").shouldBe(visible, Duration.ofSeconds(30));
 
         $(".brokers-user").shouldBe(visible, Duration.ofSeconds(10)).hover();
-        $$("th").findBy(text("Make")).shouldBe(visible);
         $(".brokers-user").click();
         $("body").click();
         $("#OTR").shouldHave(text("OTR"));
