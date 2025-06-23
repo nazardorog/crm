a9d6d700af6ea8fd2275a9cf74e50c30c95cc83b
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-06-23 13:44:22 +0300

    6

--- src/test/java/web/expedite/ui/WEU002_Broker.java
+++ src/test/java/web/expedite/ui/WEU002_Broker.java
@@ -9,6 +9,7 @@ import utilsWeb.configWeb.GlobalLogin;
 
 import java.io.File;
 import java.time.Duration;
+import java.util.Arrays;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -28,6 +29,16 @@ public class WEU002_Broker {
         System.out.println("RUN_ENV = " + runEnv);
         System.out.println("Allure reports will be saved 2: " + Configuration.reportsFolder);
 
+        // Створюємо папку, якщо не існує
+        new File("target/allure-results").mkdirs();
+
+        // Налаштовуємо Allure
+        System.setProperty("allure.results.directory", "target/allure-results");
+        Configuration.reportsFolder = "target/allure-results";
+
+        System.out.println("Allure results будуть збережені в: " +
+                new File("target/allure-results").getAbsolutePath());
+
         $(".logo-mini-icon").shouldBe(visible, Duration.ofSeconds(30));
 
         $(".brokers-user").shouldBe(visible, Duration.ofSeconds(10)).hover();
@@ -42,6 +53,25 @@ public class WEU002_Broker {
 
         System.out.println("Allure report path: " + new File(Configuration.reportsFolder).getAbsolutePath());
         System.out.println("Прилетіли");
+        System.out.println("=== ДІАГНОСТИКА ALLURE ===");
+        System.out.println("Configuration.reportsFolder: " + Configuration.reportsFolder);
+        System.out.println("System property allure.results.directory: " +
+                System.getProperty("allure.results.directory"));
+
+        File targetAllure = new File("target/allure-results");
+        File rootAllure = new File("allure-results");
+
+        System.out.println("target/allure-results існує: " + targetAllure.exists());
+        System.out.println("allure-results існує: " + rootAllure.exists());
+
+        if (targetAllure.exists()) {
+            System.out.println("Файли в target/allure-results: " +
+                    Arrays.toString(targetAllure.list()));
+        }
+        if (rootAllure.exists()) {
+            System.out.println("Файли в allure-results: " +
+                    Arrays.toString(rootAllure.list()));
+        }
     }
 
     @AfterMethod(alwaysRun = true)
