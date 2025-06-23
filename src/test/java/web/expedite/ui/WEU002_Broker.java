c677b8384d112ce5a29af2c40e33c4f3d54b70f9
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-06-23 17:08:37 +0300

    4

--- src/test/java/web/expedite/ui/WEU002_Broker.java
+++ src/test/java/web/expedite/ui/WEU002_Broker.java
@@ -44,6 +44,7 @@ public class WEU002_Broker {
         $(".brokers-user").shouldBe(visible, Duration.ofSeconds(10)).hover();
         $(".brokers-user").click();
         $("body").click();
+        $(".col-md-2.trucks-head-block-search .control-label").shouldHave(text("Truck Number Length"));
         System.out.println("Половинка тесту юхуу");
         $("#OTR").shouldHave(text("OTR"));
         $(".content-header").shouldHave(text("Brokers"));
