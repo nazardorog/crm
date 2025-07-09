309ada779a3cf5c665f4214ebced0857089f27be
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-07-09 08:33:17 +0300

    224

--- src/test/java/web/expedite/smoke/loadBoard/WES001_LoadCreateBol.java
+++ src/test/java/web/expedite/smoke/loadBoard/WES001_LoadCreateBol.java
@@ -27,6 +27,8 @@ public class WES001_LoadCreateBol {
     public void createBol() {
 
         System.out.println("Тест старт WES001_LoadCreateBol");
+        System.out.println("Тест має впасти WES001_LoadCreateBol");
+        $("#loads-form-create").shouldBe(visible, EXPECT_GLOBAL);
 
 //        // Login
 //        GlobalLogin.login("exp_disp1");
