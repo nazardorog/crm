17c002688ac6fd77c0fc4437e261ca25aa9290f8
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-07-09 10:08:58 +0300

    224

--- src/test/java/web/expedite/smoke/loadBoard/WES001_LoadCreateBol.java
+++ src/test/java/web/expedite/smoke/loadBoard/WES001_LoadCreateBol.java
@@ -30,7 +30,6 @@ public class WES001_LoadCreateBol {
 
         // Login
         GlobalLogin.login("exp_disp1");
-        System.out.println("Тест фініш WES001_LoadCreateBol");
 
         // Data for creating a load
         String atBroker = "at_Broker1";
