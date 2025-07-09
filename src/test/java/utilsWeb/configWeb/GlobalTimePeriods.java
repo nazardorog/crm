d63a5c28a4fde5c352a0b0b0392d77e97a412aa7
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-07-09 09:49:03 +0300

    224

--- src/test/java/utilsWeb/configWeb/GlobalTimePeriods.java
+++ src/test/java/utilsWeb/configWeb/GlobalTimePeriods.java
@@ -3,7 +3,7 @@ package utilsWeb.configWeb;
 import java.time.Duration;
 
 public class GlobalTimePeriods {
-    public static Duration EXPECT_GLOBAL = Duration.ofSeconds(20);
+    public static Duration EXPECT_GLOBAL = Duration.ofSeconds(40);
     public static Duration EXPECT_10 = Duration.ofSeconds(10);
     public static Duration EXPECT_5 = Duration.ofSeconds(5);
 }
