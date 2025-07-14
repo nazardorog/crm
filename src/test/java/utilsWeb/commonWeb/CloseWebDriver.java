0aa76469be09cfc2f4a5eeba6236eb3b2b11fbc5
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-07-14 17:39:52 +0300

    dfsdfa

--- src/test/java/utilsWeb/commonWeb/CloseWebDriver.java
+++ src/test/java/utilsWeb/commonWeb/CloseWebDriver.java
@@ -4,6 +4,15 @@ import com.codeborne.selenide.Selenide;
 
 public class CloseWebDriver {
 
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println(">>> JVM is shutting down, closing WebDriver...");
+            try {
+                Selenide.closeWebDriver();
+            } catch (Exception ignored) {}
+        }));
+    }
+
     public static void tearDown() {
         Selenide.closeWebDriver();
     }
