c391320a11dd87d671f83264adb1cae4b49ae4c2
Nazar PC <nazar.mykytynskiy@geekqlick.com>
2025-08-07 16:51:44 +0300

    3452345

--- src/test/java/web/expedite/smoke/loadBoard/WES002_LoadCreateRateConfirmation.java
+++ src/test/java/web/expedite/smoke/loadBoard/WES002_LoadCreateRateConfirmation.java
@@ -34,7 +34,7 @@ public class WES002_LoadCreateRateConfirmation {
 
         // Назва класу для Allure
         System.out.println("Тест2 розпочато");
-//        CustomName.getDescription();
+        CustomName.getDescription();
 
         // Login
         GlobalLogin.login("exp_disp1");
